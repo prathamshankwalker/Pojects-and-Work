c8e3b3bb6f4a34872e2943666feae7c8a5410a38
Pratham Shankwalker <79761084+prathamshankwalker@users.noreply.github.com>
2023-01-03 18:01:24 +0530

    Delete credit directory

--- credit/partials/_dbconnet.php
+++
@@ -1,9 +0,0 @@
-<?php $servername = "localhost";
-      $username = "root";
-      $password = "";
-      $database = "credit";
-
-      // Create a connection
-      $conn = mysqli_connect($servername, $username, $password, $database);
-
-      ?>
